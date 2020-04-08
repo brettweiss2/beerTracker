b3e73b73cc5200c5149649923a653908ab33b653
chenkai0930 <kchen10@stevens.edu>
2020-04-08 11:31:34 -0400

    beers route done most

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const beerRoutes = require('./beers');
+const userRoutes = require('./users');
+
+const constructorMethod = (app) => {
+	app.use('/beers', beerRoutes);
+    // app.use('/users', userRoutes);
+
+    app.get('/',(req, res) =>{
+        res.render('beers/index');
+    })
+
+
+	app.use('*', (req, res) => {
+		res.sendStatus(404);
+	});
+};
+
+module.exports = constructorMethod;
